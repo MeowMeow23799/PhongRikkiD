e9211fd27c0cebb634849ea9c94390eca7a2519b
MeowMeow23799 <52699784+MeowMeow23799@users.noreply.github.com>
2019-07-10 16:13:36 +0700

    Create Numeric.java

---
+++ Numeric.java
@@ -0,0 +1,13 @@
+public class Numeric {
+	public static void main(String arg[]){
+		int x;
+		x=10/3;
+		double y;
+		y=10/3;//ket qua la double
+		System.out.println(x);
+		System.out.println(y);
+		y=10;
+		y=y/3;//khi so chia la double
+		System.out.println(y);
+	}
+}
